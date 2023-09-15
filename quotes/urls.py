423a3c6ccffab4e1ce4bceadb78ce02cb51033da
AntonHran <anton.hranovskyi@gmail.com>
2023-09-15 20:36:25 +0300

    changes according to the task of homework 13

--- quotes/urls.py
+++ quotes/urls.py
@@ -8,7 +8,7 @@ app_name = QuotesConfig.name
 
 urlpatterns = [
     path("", views.main, name="index"),
-    path("<int:page>", views.main, name="root_paginate"),
+    path("<int:page>/", views.main, name="index"),
     path("add_author/", views.add_author, name="add_author"),
     path("author/<int:author_id>/", views.get_author, name="get_author"),
     path("add_quote1/", views.add_quote_1, name="add_quote1"),
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
 from django.contrib import messages
 
@@ -23,5 +26,14 @@ class RegisterView(View):
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect(to="")
+            return redirect(to="/")
         return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):  # !!!!
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
